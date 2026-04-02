b1b8c3b4e62e4a4f0e95eb9dfabea25bb3ea07dc
athot <athotmangkung@gmail.com>
2026-04-02 09:46:49 +0530

    leave changes

--- frontend/src/pages/employees/EmployeeLeaves.jsx
+++ frontend/src/pages/employees/EmployeeLeaves.jsx
@@ -17,31 +17,38 @@ export default function EmployeeLeaves() {
       setAllData(data);
     }
   };
+  console.log(allData.length);
   return (
     <Layout>
       <h1 className="text-xl font-bold mb-4">My Leaves</h1>
 
       <div className="grid gap-4">
-        {allData.map((l, i) => (
-          <div key={i} className="bg-white p-4 rounded-xl shadow">
-            <p className="font-bold mb-2">{l.leaveType}</p>
-            <p className="text-gray-500 mb-2">
-              {formatDate(l.fromDate)} → {formatDate(l.toDate)}
-            </p>
+        {allData?.length === 0 ? (
+          <h1 className="text-gray-600">
+            You haven’t applied for any leave yet
+          </h1>
+        ) : (
+          allData.map((l, i) => (
+            <div key={i} className="bg-white p-4 rounded-xl shadow">
+              <p className="font-bold mb-2">{l.leaveType}</p>
+              <p className="text-gray-500 mb-2">
+                {formatDate(l.fromDate)} → {formatDate(l.toDate)}
+              </p>
 
-            <span
-              className={`px-2 py-1 rounded text-sm ${
-                l.status === "APPROVED"
-                  ? "bg-green-100 text-green-600"
-                  : l.status === "REJECTED"
-                    ? "bg-red-100 text-red-600"
-                    : "bg-yellow-100 text-yellow-600"
-              }`}
-            >
-              {l.status}
-            </span>
-          </div>
-        ))}
+              <span
+                className={`px-2 py-1 rounded text-sm ${
+                  l.status === "APPROVED"
+                    ? "bg-green-100 text-green-600"
+                    : l.status === "REJECTED"
+                      ? "bg-red-100 text-red-600"
+                      : "bg-yellow-100 text-yellow-600"
+                }`}
+              >
+                {l.status}
+              </span>
+            </div>
+          ))
+        )}
       </div>
     </Layout>
   );
